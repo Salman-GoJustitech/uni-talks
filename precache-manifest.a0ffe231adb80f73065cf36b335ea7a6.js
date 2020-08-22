abc36b8c0de03f83969656524078d5266bc2de19
salmanasif0007 <salmanasif0007@gmail.com>
2020-08-22 14:08:40 +0600

    Updates

---
+++ precache-manifest.a0ffe231adb80f73065cf36b335ea7a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed98c3aed0dd07bacd5df6e3367092b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5bbf61489d4d3935f23",
+    "url": "/static/css/main.f442dd3e.chunk.css"
+  },
+  {
+    "revision": "bc2d23f1221db1979f59",
+    "url": "/static/js/2.abd7e30a.chunk.js"
+  },
+  {
+    "revision": "f84ca91f9cfb32f0c54148d930a25ad4",
+    "url": "/static/js/2.abd7e30a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5bbf61489d4d3935f23",
+    "url": "/static/js/main.a856cbe3.chunk.js"
+  },
+  {
+    "revision": "44c88f9a1a7abaf99376",
+    "url": "/static/js/runtime-main.2671ab92.js"
+  }
+]);
